284502458cfc39e3154db3e5aa2683a0674e7943
ahmed ashraf fayeq <ahmedashraffayeq@gmail.com>
2021-11-29 14:54:16 +0200

    thread types

--- GCDDemo/ViewController.swift
+++ GCDDemo/ViewController.swift
@@ -12,8 +12,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        queueType()
     }
 
+    func queueType(){
+        //by default it's serial queue without attribute
+        let queue = DispatchQueue(label: "com.fayeq.main", attributes: .concurrent)
+        queue.async {
+            for i in 100..<110{
+                print("👀", i)
+            }
+        }
+        
+        queue.async {
+            for i in 200..<210{
+                print("😝", i)
+            }
+        }
+    }
 
 }
 
